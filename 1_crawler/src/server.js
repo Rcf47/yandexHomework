4912e48c657ca2be1653b5d930e94edf8cba64ee
Vadim Cherepanov <xenos26@gmail.com>
2023-06-11 16:19:08 +0700

    chore: add code crawler

---
+++ 1_crawler/src/server.js
@@ -0,0 +1,55 @@
+import express, { json } from 'express';
+import { fetch } from 'fetcher';
+
+const app = express();
+
+app.use(json());
+
+app.post('/parse', async (req, res) => {
+  const { domainName } = req.body;
+  const visited = new Set();
+  const links = [];
+  const initialUrlArray = [`http://${domainName}`];
+
+  while (initialUrlArray.length > 0) {
+    const currentUrl = initialUrlArray.pop();
+
+    if (visited.has(currentUrl)) {
+      continue;
+    }
+
+    visited.add(currentUrl);
+
+    try {
+      const html = await fetch(currentUrl);
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+      const anchorTags = doc.getElementsByTagName('a');
+
+      for (let i = 0; i < anchorTags.length; i++) {
+        const link = anchorTags[i].getAttribute('href');
+
+        if (link.startsWith('/')) {
+          links.push(`http://${domainName}${link}`);
+        } else if (link.startsWith('http')) {
+          const { hostname } = new URL(link);
+
+          if (hostname === domainName) {
+            links.push(link);
+          }
+        }
+      }
+
+      initialUrlArray.push(...links.filter(link => !visited.has(link)));
+      links.length = 0;
+    } catch (error) {
+      console.error(`Error crawling ${currentUrl}: ${error.message}`);
+    }
+  }
+
+  res.json([...new Set(links)]);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
